7626b67f17b58227292ccec6f5806d65c60d7ec0
Animart Taipan <ov.animarttaipan@gmail.com>
2023-02-10 23:07:30 +0300

    zadanie
    v.1

---
+++ Currency_Panel/Currency_Panel/AboutForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Currency_Panel
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(418, 422);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(145, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 398);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Программа для практике 4го курса \r\nИП-19-7к Чеблаков\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r" +
+    "\n\r\n\r\n\r\n\r\n\r\n";
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 422);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "AboutForm";
+            this.Text = "AboutForm";
+            this.mainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Label label1;
+        public Panel mainPanel;
+    }
+}
---
+++ Currency_Panel/Currency_Panel/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency_Panel
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private Control oldParent;
+        private Panel oldPanel;
+
+        private void ShowExchangeForm()
+        {
+            var exchangeForm = new ExchangeForm();
+            SetContent(exchangeForm.GetPanel());
+            exchangeForm.ShowCurrencyData();
+            exchangeForm.ShowFavouriteData();
+            exchangeForm.SetStarterMark();
+        }
+
+        private void SetContent(Panel targetPanel)
+        {
+            if (oldPanel != null)
+            {
+                oldPanel.Parent = oldParent;
+                if (oldParent is Form oldParentForm)
+                {
+                    oldParentForm.Close();
+                    oldParentForm.Dispose();
+                }
+            }
+
+            var targetParent = targetPanel.Parent;
+            targetPanel.Parent = this.mainPanel;
+
+            oldPanel = targetPanel;
+            oldParent = targetParent;
+        }
+
+        private void aboutProgrammToolStripMI_Click(object sender, EventArgs e)
+        {
+            var aboutForm = new AboutForm();
+            SetContent(aboutForm.GetPanel());
+        }
+
+        private void exchangeToolStripMI_Click(object sender, EventArgs e)
+        {
+            ShowExchangeForm();
+        }
+    }
+}
